4bbd27667a1e83115b4cd2c3bd230543fbd12009
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-03 21:01:24 +0500

    Add src\components\Footer.js

---
+++ src\components\Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+const Footer = () => {
+  return (
+    <Container>
+      <Row>
+        <Col xs="12" sm="12" md="12" lg="12" xl="12">
+          <p>Copyright 2023 Ecommerce Website</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Footer;
