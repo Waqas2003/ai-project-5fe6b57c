d3560f1ef24041f39e161b253be04d4765441687
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-03 21:01:07 +0500

    Add ecommerce\settings.py

---
+++ ecommerce\settings.py
@@ -0,0 +1,24 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'ecommerce.apps.EcommerceConfig',
+    'rest_framework',
+    'django_filters',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
